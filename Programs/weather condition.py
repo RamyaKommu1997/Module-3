6603b5d456a14dee6b1d4be003208f069e54192e
RamyaKommu1997 <ramyakommu2002@gmail.com>
2026-09-17 21:45:59 +0530

    weather condition

---
+++ Programs/weather condition.py
@@ -0,0 +1,12 @@
+weather = input("Enter the weather condition: ")
+
+if weather == "sunny":
+    print("It's a sunny day!")
+elif weather == "rainy":
+    print("Take an umbrella.")
+elif weather == "cloudy":
+    print("The sky is cloudy.")
+elif weather == "cold":
+    print("Wear warm clothes.")
+else:
+    print("Weather condition not recognized.")
